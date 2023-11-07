ed1abcdcb03311ad5d0f19f2e6a429116de9fb91
tejaswini-rb <tejaswini.ramkumar@gmail.com>
2023-11-07 10:33:33 -0500

    collision checker with loser screen bug

--- app/src/main/java/com/example/sprint1/Models/Collision.java
+++ app/src/main/java/com/example/sprint1/Models/Collision.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 
 import com.example.sprint1.ViewModels.EndScreenLoser;
 
+
+
 public class Collision {
     public static void checkCollision(Context context, Player player, Enemy enemy1, Enemy enemy2) {
         if (isColliding(player, enemy1) || isColliding(player, enemy2)) {
